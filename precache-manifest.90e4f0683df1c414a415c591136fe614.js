3f56eb45731b8c938adaa4aa0432320ed906949c
Jan <janegge1990@gmail.com>
2020-03-04 21:31:33 +0100

    Updates

---
+++ precache-manifest.90e4f0683df1c414a415c591136fe614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1292d14e9d57499bd0f44c268c6ea5",
+    "url": "/Production/index.html"
+  },
+  {
+    "revision": "5898e33552e12b8e41ca",
+    "url": "/Production/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "92fc037f67f2f6159639",
+    "url": "/Production/static/js/2.d5802278.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Production/static/js/2.d5802278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5898e33552e12b8e41ca",
+    "url": "/Production/static/js/main.90f68b91.chunk.js"
+  },
+  {
+    "revision": "cfbd18995eecc93b1e0d",
+    "url": "/Production/static/js/runtime-main.257f5940.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Production/static/media/logo.5d5d9eef.svg"
+  }
+]);
